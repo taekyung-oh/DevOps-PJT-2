2ee14768234c82bb15fb2d8d4da13847792f2591
taekyung-oh <dkrldkdwk0@gmail.com>
2023-05-01 10:09:35 +0900

    Feat: Add cors & test api
    1. cors 추가
    2. test api 추가

--- helloworld-was/plugins/mongodb.js
+++ helloworld-was/plugins/mongodb.js
@@ -6,7 +6,6 @@ const { MONGO_HOST, MONGO_PORT, MONGO_USERNAME, MONGO_PASSWORD } = process.env
 
 module.exports = fp(async function (fastify, opts) {
   const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/?authMechanism=DEFAULT`  
-  console.log(url)
 
   fastify.register(require('@fastify/mongodb'), {
     forceClose: true,
